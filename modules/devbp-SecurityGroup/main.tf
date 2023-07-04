9253627e3c9d72dcba88813e77c83d3355b5aa19
Ken Phetmany <48663497+KenPhetmany@users.noreply.github.com>
2023-07-04 13:39:40 +1000

    Update main.tf

--- modules/devbp-SecurityGroup/main.tf
+++ modules/devbp-SecurityGroup/main.tf
@@ -9,7 +9,7 @@ resource "aws_security_group" "security_group" {
 resource "aws_vpc_security_group_egress_rule" "egress_rules" {
   security_group_id = aws_security_group.security_group.id
 
-  for_each   = var.sg_ingress_rules
+  for_each   = var.sg_egress_rules
   from_port   = each.value.from_port
   to_port     = each.value.to_port
   ip_protocol = each.value.protocol
@@ -19,7 +19,7 @@ resource "aws_vpc_security_group_egress_rule" "egress_rules" {
 resource "aws_vpc_security_group_ingress_rule" "ingress_rules" {
   security_group_id = aws_security_group.security_group.id
 
-  for_each   = var.sg_egress_rules
+  for_each   = var.sg_ingress_rules
   from_port   = each.value.from_port
   to_port     = each.value.to_port
   ip_protocol = each.value.protocol
